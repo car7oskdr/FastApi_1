2dcdc25c034b99977120be602b54899aadc28dae
car7oskdr <carloscvl7@yahoo.com.mx>
2022-04-22 02:10:13 -0500

    validations_body

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     lasta_name: str
@@ -37,12 +42,12 @@ def show_person(
         max_length=50,
         title="Person Name.",
         description="This is the person name. It's between 1 and 50 characters"
-        ),
+    ),
     age: str = Query(
         ...,
         title="Person Age.",
         description="This is the person age. It's required"
-        )
+    )
 ):
     return {name: age}
 
@@ -55,6 +60,27 @@ def show_person(
     person_id: int = Path(
         ...,
         gt = 0
-        )
+    )
 ):
     return {person_id: "It exists!"}
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt= 0
+    ),
+
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    """results = person.dict()
+    results.update(location.dict())"""
+    results = dict(person)
+    results.update(dict(location))
+
+    return results
