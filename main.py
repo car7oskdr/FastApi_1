2013294276a151a05d935808dab51f2705c79da6
car7oskdr <carloscvl7@yahoo.com.mx>
2022-04-22 01:07:04 -0500

    validations_query

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from typing import Optional
 #Pydantic
 from pydantic import BaseModel
 #FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -25,3 +26,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
